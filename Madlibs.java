65ee5137464be66eca614e3fe37ef475894b0284
Ayush Raj <bhattayushraj@gmail.com>
2023-09-22 23:42:05 +0530

    program of string concatination

---
+++ Madlibs.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Madlibs {
+    public static void main(String[] args){
+        //Creating a program that asks user for a season of the year, then a whole number , then an adjective.
+        //Use the input to complete the sentence below the output the result:
+        //"On a [Adjactive] [Season of the year] day, i drink a minimum of [whole number] cup of coffee"
+        
+        System.out.println("Started working");
+
+        //Asking user for the season of the year
+        System.out.println("Enter the season of the year :");
+        Scanner scanner = new Scanner(System.in);
+        String season = scanner.next();
+        //Asking user for the Whole number
+        System.out.println("Enter the whole number :");
+        int num = scanner.nextInt();
+        //Asking user for the Adjective
+        System.out.println("Enter the Adjective  :");
+        String adjective = scanner.next();
+        scanner.close();
+        
+        
+    }
+}
