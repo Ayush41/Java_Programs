07fa57e87251a120ff16eb6e8e4154995d53829b
Ayush Raj <bhattayushraj@gmail.com>
2023-09-22 23:07:42 +0530

    Scanner function

--- Basics.java
+++ Basics.java
@@ -1,5 +1,13 @@
+import java.util.Scanner; //Importing the Scanner function
 public class Basics{
-    public static void main(String[] args){//BLOCK OF CODE 
-        System.out.println("WOrking"); //Method
+    public static void main(String[] args){//MAIN METHOD 
+        System.out.println("Working");
+        //Getting the number of hrs worked from user;
+        System.out.println("type a number:-");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextInt();
+
+
+
     }
 }
