53c9fdc5682ddcb5b06420f3a667687c65047ac1
Ayush Raj <bhattayushraj@gmail.com>
2023-09-23 23:57:58 +0530

    Basic program

--- Ifelse.java
+++ Ifelse.java
@@ -3,11 +3,16 @@ public class Ifelse{
     public static void main(String[] args){
         System.out.println("IF Else demonstration :)");
         //IF STATEMENT 
-        //Checking ODD EVEN
+        //Checking for greater number
         System.out.println("Enter the number to check odd or even");
         Scanner scanner = new Scanner(System.in);
         //TAKING INPUT VALUE 
-        int num = scanner.nextInt();
-        
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        if(num1>num2){
+            System.out.println(num1+ "is greater than" + num2);
+        }else{
+            System.out.println("num 2 is greater");
+        }
     }
 }
