bd260a3b1dceaf97f92a60c6397cf87b5714495b
Ayush Raj <bhattayushraj@gmail.com>
2023-09-23 23:50:15 +0530

    if else syntax basicss

---
+++ Ifelse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Ifelse{
+    public static void main(String[] args){
+        System.out.println("IF Else demonstration :)");
+        //IF STATEMENT 
+        //Checking ODD EVEN
+        System.out.println("Enter the number to check odd or even");
+        Scanner scanner = new Scanner(System.in);
+        //TAKING INPUT VALUE 
+        int num = scanner.nextInt();
+        
+    }
+}
