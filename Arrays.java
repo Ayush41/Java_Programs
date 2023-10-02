d247f085d857c98321d9607e5f9d380480ef5ed7
Ayush Raj <bhattayushraj@gmail.com>
2023-10-02 16:55:11 +0530

    nested loop

--- Arrays.java
+++ Arrays.java
@@ -5,5 +5,15 @@ public class Arrays{
         for(int i =0;i<10;i++){
             System.out.println("i");
         }
+        //Outer loop
+        for(int i =1;i<=2;i++){
+            System.out.println("Outer :" + i);
+
+        for(int j=1;j<=3;j++){
+            System.out.println("Inner :" + j);
+        }
+        }
+
+
     }
 }
