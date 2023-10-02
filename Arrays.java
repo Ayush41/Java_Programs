9682630bb9da211502cc3b7e27c955959dd77e1d
Ayush Raj <bhattayushraj@gmail.com>
2023-10-02 16:51:05 +0530

    forLOOP

--- Arrays.java
+++ Arrays.java
@@ -1,5 +1,9 @@
 public class Arrays{
     public static void main(String[] args){
         System.out.println("Working...");
+        //Loops & Arrays
+        for(int i =0;i<10;i++){
+            System.out.println("i");
+        }
     }
 }
