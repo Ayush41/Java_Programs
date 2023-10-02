fe2e1acc079950085af26c01adc148f5b159a23f
Ayush Raj <bhattayushraj@gmail.com>
2023-10-02 16:58:32 +0530

    Nested LOOPS

---
+++ Loops.java
@@ -0,0 +1,17 @@
+public class Loops{
+    public static void main(String[] args){
+        System.out.println("Working...");
+        //Loops & Arrays
+        for(int i =0;i<10;i++){
+            System.out.println("i");
+        }
+        //Outer loop
+        for(int i =1;i<=2;i++){
+            System.out.println("Outer :" + i);
+
+        for(int j=1;j<=3;j++){
+            System.out.println("Inner :" + j);
+        }
+        }
+    }
+}
