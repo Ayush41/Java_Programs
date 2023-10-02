d3cd8e119b9a705e53e8c4fabc279680651b8eef
Ayush Raj <bhattayushraj@gmail.com>
2023-10-02 17:08:36 +0530

    Arrayzz

--- Loops.java
+++ Loops.java
@@ -1,6 +1,7 @@
 public class Loops{
     public static void main(String[] args){
         System.out.println("Working...");
+        
         //Loops & Arrays
         for(int i =0;i<10;i++){
             System.out.println("i");
@@ -9,9 +10,14 @@ public class Loops{
         for(int i =1;i<=2;i++){
             System.out.println("Outer :" + i);
 
-        for(int j=1;j<=3;j++){
+            for(int j=1;j<=3;j++){
             System.out.println("Inner :" + j);
+
         }
-        }
+        String[] Heros = {"Captain America","IronMan","SuperMan"}; //String Array
+        int[] Num = {12,45,67,9,57,4,65,3}; 
+        System.out.println(Heros[1]);
+        System.out.println(Num[4]);
+    }
     }
 }
