bdde3650297b14ce26b54fe0d2e0c99825fdbbdb
ayush41 <bhattayushraj@gmail.com>
2023-11-03 03:07:34 +0530

    loops

--- Loops.java
+++ Loops.java
@@ -18,6 +18,11 @@ public class Loops{
         int[] Num = {12,45,67,9,57,4,65,3}; 
         System.out.println(Heros[1]);
         System.out.println(Num[4]);
+
+//MULTI DIMENSIONAL ARRAYS
+int TwoD[][] = {{1,2,3,4}, {7,6,5,4}};
+System.out.println(TwoD[1][1]);
+
     }
     }
 }
