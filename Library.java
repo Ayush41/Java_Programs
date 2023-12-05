5bb7b2bba6a114f20c62224c5fd9c0c5a2546635
ayush41 <bhattayushraj@gmail.com>
2023-12-05 12:32:39 +0530

    convept

---
+++ Library.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Library{
+    public static void main(String args[]){
+        ArrayList<String>list = new ArrayList<String>();
+
+        list.add("Ravi");
+        list.add("Vijay");
+        list.add("Ravan");
+        list.add("Aruyan");
+        Iterator itr = list.iterator();
+        while (itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
+}
