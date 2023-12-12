cdb0b1b5f377b947492de6b5d1ae20655bf6b3d1
ayush41 <bhattayushraj@gmail.com>
2023-12-13 02:56:02 +0530

    writen

--- COLLEGE-PRACTICAL/File-Handling/Write.java
+++ COLLEGE-PRACTICAL/File-Handling/Write.java
@@ -4,6 +4,22 @@ import java.util.*;
 class Write{
     public static void main(String[] args){
 //WRTING INTO THE FILE
+try {
+    FileWriter f = new FileWriter("F:\\WebDevelpment\\Programming\\Java_Programs\\COLLEGE-PRACTICAL\\File-Handling\\write.txt"); //To Create a File 
+    try{
+        //performing write operation
+        f.write("WE FINALLY WROTE SOME DATA INTO THIS FILE NAMED AS 'WRITE.TXT'");
+    }
+    finally{
+        //closing the file after writing data into it
+        f.close();   
+    }
+    System.out.println("Succesful writen the content in file");
+
+}catch(){
+    System.out.println("Error in writing into the file");  //Printing Error Message if any error occurs while writing into the file
+}
+
 
     }
 }
