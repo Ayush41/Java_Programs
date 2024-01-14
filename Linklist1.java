9904bb49dd487ccf7cf2d28ae13247bb5457eb6e
ayush41 <bhattayushraj@gmail.com>
2024-01-14 22:44:22 +0530

    basic add and remove from linked list

--- Linklist1.java
+++ Linklist1.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 class Linklist1{
     public static void main(String[] args){
         System.out.println("LinkedList 2 ");
@@ -7,5 +9,19 @@ class Linklist1{
         //search()     : Search for a given key in the linked list
         //display()    : Displays the linked list.
         // getFirst(),removeLast(),removeFirst()
+
+        // Creating a LinkedList
+        LinkedList<String> llist = new LinkedList<>();
+        // Adding elements to the linked list using addAtHead method.
+        llist.add("Mercedes");
+        llist.add("Audi");
+        llist.add("Bucatti veyron");
+        llist.addFirst("Lamborgini");
+        llist.addLast("lastCar-Nano");
+
+        // Removign the elements;
+        llist.removeLast(); //removes the last element from the list..
+        // Displaying the linkedlist
+        System.out.println(llist);
     }
 }
