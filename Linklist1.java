75b9639f83d2c9d1783a27b8b3a3c0a0126aa99f
ayush41 <bhattayushraj@gmail.com>
2024-01-14 22:30:46 +0530

    methods

---
+++ Linklist1.java
@@ -0,0 +1,11 @@
+class Linklist1{
+    public static void main(String[] args){
+        System.out.println("LinkedList 2 ");
+        //METHODS :-
+        //addAtHead() : Adds a new node at the head of the linked list.
+        //deleteNode() : Deletes a given node from the linked list.
+        //search()     : Search for a given key in the linked list
+        //display()    : Displays the linked list.
+        // getFirst(),removeLast(),removeFirst()
+    }
+}
