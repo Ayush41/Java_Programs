9a7a1186cbb917421ae9e6d0bd01b37f9b70eac8
ayush41 <anonymousking41681@gmail.com>
2025-01-11 21:15:05 +0530

    sorting

--- 100-Programs/Arrays/find_largest_element.java
+++ 100-Programs/Arrays/find_largest_element.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
-
+import java.util.Arrays;
 public class Main{
     public static void main(String[] args) {
         
-        int arr[] = {12,1,13,5,10,5,34,9};
+        int arr[] = {12,1,13,5,10,5,99,34,9,3};
 
         int max = arr[0];
 
+        Arrays.sort(arr); //using inbuilt fnc
+        System.out.println("Sorted array with inbuilt fnc: " + Arrays.toString(arr));
         for(int i=0;i<arr.length;i++){
             if(max<arr[i]){
                 max = arr[i];
