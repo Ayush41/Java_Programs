403ccfc32839073b201826ac2e64ceb815196a60
ayush41 <anonymousking41681@gmail.com>
2025-01-12 19:43:22 +0530

    methods

--- OOPs/Method.java
+++ OOPs/Method.java
@@ -1,5 +1,10 @@
 public class Method{
     public static void main(String[] args){
         System.out.println("Program Working...");
+        //METHODS are functions in java that are executed when thay have been called.
+        //Why use Methods? 
+        //Methods Are used reuse code:- Define the code once, and use it many times.
+
+
     }
 }
